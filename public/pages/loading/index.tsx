cea7ba7adac1ada95f70dd840baa4cd09d5db581
huang.jian <hjj491229492@hotmail.com>
2020-05-24 11:55:58 +0800

    chore: initialize storyboard

---
+++ public/pages/loading/index.tsx
@@ -0,0 +1,27 @@
+// packages
+import * as React from 'react';
+import { View, Text } from 'remax/wechat';
+
+// internal
+import Loading from '../../../packages/Loading';
+
+export default () => {
+  return (
+    <View>
+      <Text className="demo-block__title">加载类型</Text>
+      <View className="demo-loading">
+        <Loading />
+        <Loading type="spinner" />
+      </View>
+      <Text className="demo-block__title">加载文案水平</Text>
+      <View className="demo-loading">
+        <Loading message="加载中..." />
+      </View>
+      <Text className="demo-block__title">加载文案垂直分布</Text>
+      <View className="demo-loading">
+        <Loading vertical message="加载中..." />
+        <Loading type="spinner" vertical message="加载中..." />
+      </View>
+    </View>
+  );
+};
