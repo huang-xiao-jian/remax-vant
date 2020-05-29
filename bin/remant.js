f8439b3f1a4975026f2cdfce71378d6004acd267
huang.jian <hjj491229492@hotmail.com>
2020-05-29 11:29:57 +0800

    chore: add quick package creation

---
+++ bin/remant.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs-extra');
+const { pascalCase } = require('pascal-case');
+const { program } = require('commander');
+
+// render split
+const renderIndex = (name) => `
+  /**
+   * @description - nothing but export component
+   */
+  export { default } from './${name}';
+`;
+const renderComponent = (name) => `
+// packages
+import React, { FunctionComponent } from 'react';
+import clsx from 'clsx';
+import { View } from 'remax/wechat';
+// internal
+import './${name}.css';
+
+// 默认值填充属性
+interface Neutral${name}Props {}
+
+interface Exogenous${name}Props {
+  // 容器类名，用以覆盖内部
+  className?: string;
+}
+
+type ${name}Props = Neutral${name}Props & Exogenous${name}Props;
+
+const ${name}: FunctionComponent<${name}Props> = (props) => {
+  const { className, children } = props;
+  const classnames = {
+    container: clsx(className),
+  };
+
+  return <View className={classnames.container}>{children}</View>;
+};
+
+export default ${name};
+`;
+
+const renderPage = (name) => `
+// packages
+import * as React from 'react';
+import { View, Text } from 'remax/wechat';
+
+// internal
+import ${name} from '../../../packages/${name}';
+
+export default () => {
+  return (
+    <View className="demo-block">
+      <Text className="demo-block__title">基础用法</Text>
+      <View className="demo-block__content">
+      </View>
+    </View>
+  );
+};
+`;
+
+program
+  .version('0.1.0')
+  .command('create <package>')
+  .description('setup package development grass')
+  .action((package) => {
+    const name = pascalCase(package);
+    const files = [
+      {
+        filename: path.resolve(__dirname, '../packages', name, 'index.ts'),
+        content: renderIndex(name),
+      },
+      {
+        filename: path.resolve(__dirname, '../packages', name, `${name}.tsx`),
+        content: renderComponent(name),
+      },
+      {
+        filename: path.resolve(__dirname, '../packages', name, `${name}.css`),
+        content: '',
+      },
+      {
+        filename: path.resolve(__dirname, '../public', package, `index.tsx`),
+        content: renderPage(name),
+      },
+    ];
+
+    files.forEach((item) => {
+      fs.ensureFile(item.filename).then(() => {
+        fs.writeFileSync(item.filename, item.content);
+      });
+    });
+  });
+
+program.parse(process.argv);
