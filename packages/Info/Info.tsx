7e25bb873367df5555ab8e26d07cd8df6302461c
huang.jian <hjj491229492@hotmail.com>
2020-06-04 15:56:22 +0800

    refactor: :recycle: adapt to standard

--- packages/Info/Info.tsx
+++ packages/Info/Info.tsx
@@ -3,13 +3,13 @@ import React, { FunctionComponent, CSSProperties } from 'react';
 import clsx from 'clsx';
 import { View } from 'remax/wechat';
 // internal
+import { Select } from '../tools/Switch';
 import './Info.css';
 
 interface InfoProps {
   // 移植属性
   dot?: boolean;
   info?: string;
-  // 调整属性
   // info#customStyle
   style?: CSSProperties;
   // 容器类名，用以覆盖内部
@@ -18,22 +18,22 @@ interface InfoProps {
 
 const Info: FunctionComponent<InfoProps> = (props) => {
   const { className, dot, info, style } = props;
+  // 原始版本 wx:if 判断条件
+  const oin = (info !== null && info !== '') || dot;
 
-  if (info || dot) {
-    const classnames = {
-      container: clsx(className, 'van-info', {
-        'van-info--dot': dot,
-      }),
-    };
+  const classnames = {
+    container: clsx(className, 'van-info', {
+      'van-info--dot': dot,
+    }),
+  };
 
-    return (
+  return (
+    <Select in={oin}>
       <View style={style} className={classnames.container}>
         {dot ? '' : info}
       </View>
-    );
-  }
-
-  return null;
+    </Select>
+  );
 };
 
 export default Info;
