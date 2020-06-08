e36d1c27b37d39ba1fbada29f37579185d791a7a
huang.jian <hjj491229492@hotmail.com>
2020-06-08 10:59:34 +0800

    refactor: :recycle: optimize code detail

--- packages/Steps/Steps.tsx
+++ packages/Steps/Steps.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 import { View } from 'remax/wechat';
 // internal
 import Icon from '../Icon';
+import { Select } from '../tools/Switch';
 import withDefaultProps from '../tools/with-default-props-advance';
 import pickStyle from '../tools/pick-style';
 import './Steps.css';
@@ -19,7 +20,7 @@ interface NeutralStepsProps {
   direction: 'horizontal' | 'vertical';
   activeColor: string;
   inactiveColor: string;
-  // 自定义图标占位
+  // 自定义图标占位，自定义图标需要颜色适配，当前颜色由组件内部管理，所以推荐使用函数
   renderActiveCircle: (color: string) => ReactElement;
   renderInactiveCircel: (color: string) => ReactElement;
 }
@@ -27,9 +28,10 @@ interface NeutralStepsProps {
 interface ExogenousStepsProps {
   // 步骤定义
   steps: StepItem[];
-  onClickStep?: (event: { detail: number }) => void;
   // 容器类名，用以覆盖内部
   className?: string;
+  // 事件绑定
+  onClickStep?: (event: { detail: number }) => void;
 }
 
 type StepsProps = NeutralStepsProps & ExogenousStepsProps;
@@ -71,16 +73,16 @@ const Steps: FunctionComponent<StepsProps> = (props) => {
     const status =
       // eslint-disable-next-line no-nested-ternary
       index < active ? 'finish' : index === active ? 'process' : 'inactive';
-    const styles: Record<'container' | 'title' | 'line', CSSProperties> = {
-      container: {
+    const stylesheets: Record<'container' | 'title' | 'line', CSSProperties> = {
+      container: pickStyle({
         color: status === 'inactive' ? inactiveColor : undefined,
-      },
-      title: {
+      }),
+      title: pickStyle({
         color: status === 'process' ? activeColor : undefined,
-      },
-      line: {
+      }),
+      line: pickStyle({
         backgroundColor: status === 'finish' ? activeColor : inactiveColor,
-      },
+      }),
     };
     const classNameStep = clsx(
       'van-step',
@@ -88,25 +90,11 @@ const Steps: FunctionComponent<StepsProps> = (props) => {
       `van-step--${direction}`,
       `van-step--${status}`
     );
-    // 新增 van-step__title-text，van-step__title-desc 便于样式覆盖
-    const title = (
-      <View className="van-step__title" style={pickStyle(styles.title)}>
-        <View className="van-step__title-text">{step.text}</View>
-        <View className="van-step__title-desc">{step.desc}</View>
-      </View>
-    );
     const currentColor = status === 'inactive' ? inactiveColor : activeColor;
-    const circle = (
-      <View className="van-step__circle-container">
-        {index === active
-          ? renderActiveCircle(currentColor)
-          : renderInactiveCircel(currentColor)}
-      </View>
-    );
     // 非末尾填充线条
-    const line = index !== steps.length - 1 && (
-      <View style={pickStyle(styles.line)} className="van-step__line" />
-    );
+    const visibility = {
+      line: index !== steps.length - 1,
+    };
     // 事件绑定
     const onClick = () => {
       if (typeof onClickStep === 'function') {
@@ -114,17 +102,27 @@ const Steps: FunctionComponent<StepsProps> = (props) => {
       }
     };
 
+    // 新增 van-step__title-text，van-step__title-desc 便于样式覆盖
     return (
       /* eslint-disable react/no-array-index-key */
       <View
         key={index}
-        style={pickStyle(styles.container)}
+        style={stylesheets.container}
         className={classNameStep}
         onClick={onClick}
       >
-        {title}
-        {circle}
-        {line}
+        <View className="van-step__title" style={pickStyle(stylesheets.title)}>
+          <View className="van-step__title-text">{step.text}</View>
+          <View className="van-step__title-desc">{step.desc}</View>
+        </View>
+        <View className="van-step__circle-container">
+          {index === active
+            ? renderActiveCircle(currentColor)
+            : renderInactiveCircel(currentColor)}
+        </View>
+        <Select in={visibility.line}>
+          <View style={stylesheets.line} className="van-step__line" />
+        </Select>
       </View>
     );
   });
