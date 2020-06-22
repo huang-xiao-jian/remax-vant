6f44d5865f706df6529ac29bdfb1ff8563c32d74
huang.jian <hjj491229492@hotmail.com>
2020-06-22 10:20:55 +0800

    chore: 👷use remax-vant-cli as development tool

---
+++ storyboard/app.template.ts
@@ -0,0 +1,13 @@
+import { AppConfig } from 'remax/wechat';
+
+const config: AppConfig = {
+  pages: [
+    /* Remant Inject */
+  ],
+  window: {
+    navigationBarTitleText: 'Remax Vant',
+    navigationBarBackgroundColor: '#282c34',
+  },
+};
+
+export default config;
