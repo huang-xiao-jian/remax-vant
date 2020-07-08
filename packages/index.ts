12f3ffb7e430f9e04d72900161561a90e70516f6
huang.jian <hjj491229492@hotmail.com>
2020-07-08 14:15:40 +0800

    chore: 🚀setup exports field, publish directories

---
+++ packages/index.ts
@@ -0,0 +1,50 @@
+/* eslint-disable import/prefer-default-export */
+
+// 基础组件
+export { default as Button } from './Button';
+export { default as Cell } from './Cell';
+export { default as Icon } from './Icon';
+export { default as Image } from './Image';
+export { default as Row } from './Row';
+export { default as Col } from './Col';
+export { default as Popup } from './Popup';
+export { default as Transition } from './Transition';
+
+// 表单组件
+export { default as Checkbox } from './Checkbox';
+export { default as Picker } from './Picker';
+export { default as Radio } from './Radio';
+export { default as Rate } from './Rate';
+export { default as Slider } from './Slider';
+export { default as Switch } from './Switch';
+
+// 反馈组件
+export { default as DropdownMenu } from './DropdownMenu';
+export { default as Loading } from './Loading';
+export { default as Overlay } from './Overlay';
+export * from './Notify';
+export * from './Toast';
+
+// 展示组件
+export { default as Collapse } from './Collapse';
+export { default as Divider } from './Divider';
+export { default as NoticeBar } from './NoticeBar';
+export { default as Panel } from './Panel';
+export { default as Progress } from './Progress';
+export { default as Skeleton } from './Skeleton';
+export { default as Steps } from './Steps';
+export { default as Sticky } from './Sticky';
+export { default as Tag } from './Tag';
+export { default as TreeSelect } from './TreeSelect';
+
+// 导航组件
+export { default as Grid } from './Grid';
+export { default as IndexBar } from './IndexBar';
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as Tabbar } from './Tabbar';
+
+// 业务组件
+export { default as Card } from './Card';
+export { default as SubmitBar } from './SubmitBar';
+export { default as GoodsAction } from './GoodsAction';
